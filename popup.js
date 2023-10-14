1bc18cc2192ca6783f7697e5c3f9b9e1fadeb197
Grigory Shatalin <grigory.shatalin@akvelon.com>
2023-10-14 15:18:50 -0700

    Very basic working chrome extension

---
+++ popup.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Close the pop-up when the "Close" button is clicked
+    document.getElementById("closeButton").addEventListener("click", function () {
+        window.close();
+    });
+});
